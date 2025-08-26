624b0c59ad766ba1e79569e59a5d2831522c2496
Erik Kauê <kaue_erik122@hotmail.com>
2025-08-26 19:46:24 -0300

    Expo Router - Passando parâmetro para a rota

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -7,6 +7,10 @@ export default function Index() {
       <Text>Hello Expo Router</Text>
 
       <Button title="Go to Target" onPress={() => router.navigate("/target")} />
+      <Button
+        title="Transaction"
+        onPress={() => router.navigate("/transaction/13")}
+      />
     </View>
   );
 }
