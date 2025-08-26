a5515644a26fc2bf114991da642d9fbc087b5337
Erik Kauê <kaue_erik122@hotmail.com>
2025-08-26 19:54:07 -0300

    Expo Router - Criando a rota In Progress

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,10 +6,16 @@ export default function Index() {
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Hello Expo Router</Text>
 
-      <Button title="Go to Target" onPress={() => router.navigate("/target")} />
+      <Button title="Nova Meta" onPress={() => router.navigate("/target")} />
+
       <Button
         title="Transaction"
         onPress={() => router.navigate("/transaction/13")}
+      />
+
+      <Button
+        title="Progresso"
+        onPress={() => router.navigate("/in-progress/234")}
       />
     </View>
   );
