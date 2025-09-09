00ed72b9a2b4f64a2c6db5025943935afba82f13
Erik Kauê <kaue_erik122@hotmail.com>
2025-09-09 20:20:55 -0300

    Criando a Interface - LinearGradient

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,24 +1,10 @@
-import { router } from "expo-router";
-import { Button, Text, View } from "react-native";
-
-import { fontFamily } from "@/theme/fontFamily";
+import { HomeHeader } from "@/components/HomeHeader";
+import { View } from "react-native";
 
 export default function Index() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ fontFamily: fontFamily.bold }}>Hello Expo Router</Text>
-
-      <Button title="Nova Meta" onPress={() => router.navigate("/target")} />
-
-      <Button
-        title="Transaction"
-        onPress={() => router.navigate("/transaction/13")}
-      />
-
-      <Button
-        title="Progresso"
-        onPress={() => router.navigate("/in-progress/234")}
-      />
+    <View style={{ flex: 1 }}>
+      <HomeHeader />
     </View>
   );
 }
