4526a923b56222e5c9409bd324b7b678640d9945
Erik Kauê <kaue_erik122@hotmail.com>
2025-09-09 21:38:04 -0300

    Criando a Interface - Componente Button

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,10 @@
 import { View } from "react-native";
+import { router } from "expo-router";
 
 import { HomeHeader } from "@/components/HomeHeader";
 import { Target } from "@/components/Target";
 import { List } from "@/components/List";
+import { Button } from "@/components/Button";
 
 const summary = {
   total: "2.680,00",
@@ -43,10 +45,19 @@ export default function Index() {
         title="Metas"
         data={targets}
         keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <Target data={item} />}
+        renderItem={({ item }) => (
+          <Target
+            data={item}
+            onPress={() => router.navigate(`/in-progress/${item.id}`)}
+          />
+        )}
         emptyMessage="Nenhuma meta cadastrada"
         containerStyle={{ paddingHorizontal: 24 }}
       />
+
+      <View style={{ padding: 24, paddingBottom: 32 }}>
+        <Button title="Nova Meta" onPress={() => router.navigate("/target")} />
+      </View>
     </View>
   );
 }
