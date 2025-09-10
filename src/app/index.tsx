0dde794ee9c055534ff4b8a3a730cf97e0bfc8e3
Erik Kauê <kaue_erik122@hotmail.com>
2025-09-09 21:09:00 -0300

    Criando a interface - Componente Target

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,7 @@
 import { View } from "react-native";
 
 import { HomeHeader } from "@/components/HomeHeader";
+import { Target } from "@/components/Target";
 
 const summary = {
   total: "2.680,00",
@@ -8,10 +9,33 @@ const summary = {
   output: { label: "Saídas", value: "-R$ 883,65" },
 };
 
+const targets = [
+  {
+    id: "1",
+    name: "Viagem para o Japão",
+    percentage: "40%",
+    current: "R$ 2.680,00",
+    target: "R$ 6.700,00",
+  },
+  {
+    id: "2",
+    name: "Fazer um curso de React Native",
+    percentage: "70%",
+    current: "R$ 1.400,00",
+    target: "R$ 2.000,00",
+  },
+];
+
 export default function Index() {
   return (
     <View style={{ flex: 1 }}>
       <HomeHeader data={summary} />
+      <Target
+        data={targets[0]}
+      />
+      <Target
+        data={targets[1]}
+      />
     </View>
   );
 }
