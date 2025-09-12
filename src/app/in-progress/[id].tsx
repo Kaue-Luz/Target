e0b3fbb5e24403e71419c565772fa7f1f0a5c0e6
Erik Kauê <kaue_erik122@hotmail.com>
2025-09-11 21:45:43 -0300

    Criando a Interface - Componente Transaction

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -3,12 +3,33 @@ import { View } from "react-native";
 
 import { PageHeader } from "@/components/PageHeader";
 import { Progress } from "@/components/Progress";
+import { List } from "@/components/List";
+import { Transaction, TransactionProps } from "@/components/Transaction";
+
+import { TransactionTypes } from "@/utils/TransactionTypes";
 
 const details = {
   current: "R$ 50,00",
   target: "R$ 100,00",
   percentage: 50,
 };
+
+const transactions: TransactionProps[] = [
+  {
+    id: "1",
+    value: "R$ 50,00",
+    date: "2024-06-01",
+    description: "Grocery shopping",
+    type: TransactionTypes.INPUT,
+  },
+  {
+    id: "2",
+    value: "R$ 30,00",
+    date: "2024-06-02",
+    description: "Transport",
+    type: TransactionTypes.OUTPUT,
+  },
+];
 
 export default function InProgress() {
   const params = useLocalSearchParams<{ id: string }>();
@@ -20,6 +41,14 @@ export default function InProgress() {
         rightButton={{ icon: "edit", onPress: () => {} }}
       />
       <Progress data={details} />
+
+      <List
+        title="Transações"
+        data={transactions}
+        renderItem={({ item }) => (
+          <Transaction data={item} onRemove={() => {}} />
+        )}
+      />
     </View>
   );
 }
