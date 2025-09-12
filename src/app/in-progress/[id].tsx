1fe3fcb59075643f136602a3f6a2c74dff7a0da3
Erik Kauê <kaue_erik122@hotmail.com>
2025-09-11 21:18:41 -0300

    Criando a Interface - Componente Progress

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -1,14 +1,25 @@
-import { router, useLocalSearchParams } from "expo-router";
-import { Button, Text, View } from "react-native";
+import { useLocalSearchParams } from "expo-router";
+import { View } from "react-native";
+
+import { PageHeader } from "@/components/PageHeader";
+import { Progress } from "@/components/Progress";
+
+const details = {
+  current: "R$ 50,00",
+  target: "R$ 100,00",
+  percentage: 50,
+};
 
 export default function InProgress() {
   const params = useLocalSearchParams<{ id: string }>();
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>ID: {params.id}</Text>
-
-      <Button title="Go back" onPress={() => router.back()} />
+    <View style={{ flex: 1, padding: 24, gap: 32 }}>
+      <PageHeader
+        title="In Progress"
+        rightButton={{ icon: "edit", onPress: () => {} }}
+      />
+      <Progress data={details} />
     </View>
   );
 }
